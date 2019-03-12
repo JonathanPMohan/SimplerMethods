af392c328358b768c9ccb41b07f4d7387802a94c
unknown <jonathanmohan.net@gmail.com>
2019-03-12 16:15:19 -0500

    simpler methods

---
+++ SimplerMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimplerMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dungBeetle = new Bug("dungBeetle", "OtherName",
+                new List<string> { "ibis", "crows", "foxes" },
+                new List<string> { "dung", "more dung", "roasted dung", "dung dessert" });
+
+            Console.WriteLine(dungBeetle.PreyList());
+            Console.WriteLine(dungBeetle.PredatorList());
+            Console.WriteLine(dungBeetle.Eat("crows"));
+            Console.WriteLine(dungBeetle.Eat("dung"));
+            Console.ReadKey();
+        }
+    }
+
+}
